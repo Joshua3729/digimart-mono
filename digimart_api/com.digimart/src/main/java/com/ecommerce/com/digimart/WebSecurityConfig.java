933359f5be09f9f828274bfd12ecbea8a6b59f25
Joshua Khumalo <3729344@myuwc.ac.za>
2024-07-28 00:32:58 +0200

    updated security filter chain

--- digimart_api/com.digimart/src/main/java/com/ecommerce/com/digimart/WebSecurityConfig.java
+++ digimart_api/com.digimart/src/main/java/com/ecommerce/com/digimart/WebSecurityConfig.java
@@ -19,6 +19,7 @@ public class WebSecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/products").permitAll()
                         .requestMatchers("/products/**").permitAll()      //Existing services
                         .requestMatchers(HttpMethod.OPTIONS).permitAll()      //PBE
                         .requestMatchers("/api/**").authenticated());
