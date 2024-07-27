f52fd84f1cad47b6c4b4ea9b3145aec9a65b4c20
Joshua Khumalo <3729344@myuwc.ac.za>
2024-07-28 00:33:51 +0200

    updated get all products

--- digimart_api/com.digimart/src/main/java/com/ecommerce/com/digimart/services/impl/ProductService.java
+++ digimart_api/com.digimart/src/main/java/com/ecommerce/com/digimart/services/impl/ProductService.java
@@ -2,6 +2,8 @@ package com.ecommerce.com.digimart.services.impl;
 
 import com.ecommerce.com.digimart.entities.Product;
 import com.ecommerce.com.digimart.services.base.IProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -9,12 +11,15 @@ import java.util.List;
 @Service
 public class ProductService implements IProductService
 {
+    @Autowired
+    private com.ecommerce.com.digimart.dbms.ProductServiceDBMS DBMSProductService;
+
     @Override
-    public List<Product> getAllProducts()
+    public List<Product> getAllProducts(String category)
     {
-        Product product = new Product();
-        Product[] items = new Product[] {product};
-        return List.of(items);
+        List<Product> products = DBMSProductService.getProductsByCategory(category);
+
+        return products;
     }
 
     @Override
