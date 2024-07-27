355a1ae37afd7a1394b700a16950349f083078a5
Joshua Khumalo <3729344@myuwc.ac.za>
2024-07-28 00:34:27 +0200

    Added dbms product service

---
+++ digimart_api/com.digimart/src/main/java/com/ecommerce/com/digimart/dbms/ProductServiceDBMS.java
@@ -0,0 +1,21 @@
+package com.ecommerce.com.digimart.dbms;
+
+import com.ecommerce.com.digimart.dbms.persistance.repositories.ProductRepository;
+import com.ecommerce.com.digimart.entities.Product;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@Log4j2
+public class ProductServiceDBMS {
+    @Autowired
+    ProductRepository productRepository;
+
+    public List<Product> getProductsByCategory(String category)
+    {
+        return productRepository.findAllByCategory(category);
+    }
+}
